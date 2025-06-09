36229111d284c1a0e3b16c88667cc37842473712
pacodinucci <franciscoldinucci@gmail.com>
2025-06-09 20:42:33 -0300

    testing code rabbit 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen w-full flex justify-center items-center text-4xl text-green-500 font-bold">
       Hello World!
-      <h3>Testing code rabbit</h3>
+      <h3>Testing code rabbit!!!</h3>
     </div>
   );
 }
