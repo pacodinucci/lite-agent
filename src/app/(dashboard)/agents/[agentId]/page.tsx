385dddc462ceba53f18a36135046de57a2915704
pacodinucci <franciscoldinucci@gmail.com>
2025-06-22 22:30:57 -0300

    agent id page update

---
+++ src/app/(dashboard)/agents/[agentId]/page.tsx
@@ -0,0 +1,36 @@
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import {
+  AgentIdView,
+  AgentIdViewError,
+  AgentIdViewLoading,
+} from "@/modules/agents/ui/views/agent-id-view";
+
+interface AgentIdPageProps {
+  params: Promise<{ agentId: string }>;
+}
+
+const AgentIdPage = async ({ params }: AgentIdPageProps) => {
+  const { agentId } = await params;
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.agents.getOne.queryOptions({
+      id: agentId,
+    })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<AgentIdViewLoading />}>
+        <ErrorBoundary fallback={<AgentIdViewError />}>
+          <AgentIdView agentId={agentId} />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
+
+export default AgentIdPage;
